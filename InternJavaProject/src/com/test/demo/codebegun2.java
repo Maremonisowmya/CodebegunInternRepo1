9145608a1cf27b9bbe243a353f6a05a85453f585
Maremonisowmya <157139103+Maremonisowmya@users.noreply.github.com>
2024-11-15 21:13:33 +0530

    Create codebegun2.java

---
+++ InternJavaProject/src/com/test/demo/codebegun2.java
@@ -0,0 +1,17 @@
+package com.test.demo;
+
+public class CodeBegun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		System.out.println("Hello");
+
+		System.out.println("Hello CODEBEGUN");
+		System.out.println("Easy to Learn");
+		System.out.println("hello world");
+
+	}
+
+}
